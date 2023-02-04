d5d0530d962b74581889e0812f3965008580cbc9
illiakalinin <illinoiz4305@gmail.com>
2023-02-04 17:37:36 +0200

    Add store and slice

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+// 1 'https://ullhgy.sse.codesandbox.io/'
+// 2 get 'https://ullhgy.sse.codesandbox.io/contacts'
+//   post 'https://ullhgy.sse.codesandbox.io/contacts' {}
+//   patch 'https://ullhgy.sse.codesandbox.io/contacts/10' {}
+//   delete 'https://ullhgy.sse.codesandbox.io/contacts/20'
+
+const httpClient = axios.create({
+  baseURL: 'https://ullhgy.sse.codesandbox.io/'
+})
+
+const result = httpClient
+  .get('/contacts')
+  .then(data => console.log('data :>> ', data.data))
+  .catch(e => console.log('e :>> ', e))
+
+// httpClient
+//   .post('/contacts', { name: 'Test007' })
+//   .then(data => console.log('data :>> ', data.data))
+//   .catch(e => console.log('e :>> ', e))
+
+// httpClient
+//   .patch('/contacts/c0482243-ea82-4edc-a156-1093b3b904f8', { name: 'NewTest' })
+//   .then(data => console.log('data :>> ', data.data))
+//   .catch(e => console.log('e :>> ', e))
+
+// httpClient
+//   .delete('/contacts/1e4eac0b-d7f0-429c-9b31-fabca3bbc0a1')
+//   .then(data => console.log('data :>> ', data.data))
+//   .catch(e => console.log('e :>> ', e))
